72a51fc512ff98b9395f3ab94cac642018e80c2e
Iwan <99279234+iwanlothbrok@users.noreply.github.com>
2022-02-20 17:22:12 +0200

    Add CSharp Advance-Sets and Dictionaries Advanced Exercises

---
+++ CSharp-Advance/Sets and Dictionaries Advanced Exercise/02.Periodic Table/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+namespace Table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n = int.Parse(Console.ReadLine());
+           SortedSet<string> set = new SortedSet<string>();
+            
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+                foreach (string s in input)
+                {
+                    set.Add(s);
+                }
+            }
+            foreach (var output in set)
+            {
+                Console.Write(output + " ");
+            }
+        }
+    }
+}
---
+++ CSharp-Advance/Sets and Dictionaries Advanced Exercise/03.Count Symbols/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+namespace CountSymbols
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string words = Console.ReadLine();
+
+            SortedDictionary<char, int> dictonary = new SortedDictionary<char, int>();
+            for (int i = 0; i < words.Length; i++)
+            {
+                char c = words[i];
+                if (!dictonary.ContainsKey(c))
+                {
+                    dictonary.Add(c, 0);
+                }
+                dictonary[c]++;
+            }
+            foreach (var item in dictonary)
+            {
+                Console.WriteLine($"{item.Key}: {item.Value} time/s");
+            }
+        }
+    }
+}
