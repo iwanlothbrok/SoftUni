e05cf1caab047e197de986f0fa550d4e910b4469
Iwan <99279234+iwanlothbrok@users.noreply.github.com>
2022-02-26 18:16:44 +0200

    Add CSharp-Advanced/ClasesLab

---
+++ CSharp-Advanced/06ClasesLab/01.Car1/01.Car1/Program.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace CarManufacturer
+{
+   public class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+            Car car = new Car();
+            car.Make = "VW";
+            car.Model = "MK3";
+            car.Year = 1992;
+            Console.WriteLine($"Make: {car.Make}\nModel: {car.Model}\nYear: {car.Year}");
+        }
+    }
+}
