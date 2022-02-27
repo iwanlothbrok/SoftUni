95f0b4b3cda53f27dd5296d7e387ee807f1cff0c
Iwan <99279234+iwanlothbrok@users.noreply.github.com>
2022-02-27 09:23:42 +0200

    Add CSharp-Advanced/ClasesLab

---
+++ CSharp-Advanced/06ClasesLab/03.CarConstructor/Car.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Text;
+
+namespace CarManufacturer
+{
+    public class Car
+    {
+
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public double FuelQuantity { get; set; }
+        public double FuelConsumption { get; set; }
+
+
+        public Car()
+        {
+            Make = "VW";
+            Model = "Golf";
+            Year = 2025;
+            FuelQuantity = 200;
+            FuelConsumption = 10;
+        }
+        public Car(string make, string model, int year)
+                   : this()
+        {
+            this.Make = make;
+            this.Model = model;
+            this.Year = year;
+
+        }
+        public Car(string make, string model,  int year, double fuelQuantity,  double fuelConsumption)
+            :this(make,model,year)
+        {
+            this.FuelQuantity = fuelQuantity;
+            this.FuelConsumption = fuelConsumption;
+        }
+        public void Drive(double distance)
+        {
+            double totalFuel = FuelQuantity - (FuelConsumption * (distance / 100));
+            if (totalFuel > 0)
+            {
+                FuelQuantity = FuelQuantity - (FuelConsumption * (distance / 100));
+            }
+            else
+            {
+                Console.WriteLine("Not enough fuel to perform this trip!");
+            }
+        }
+        public string WhoAmI()
+        {
+            var result = new StringBuilder();
+
+            result.AppendLine($"Make: {this.Make}");
+            result.AppendLine($"Model: {this.Model}");
+            result.AppendLine($"Year: {this.Year}");
+            result.Append($"Fuel: {this.FuelQuantity:F2}L");
+
+            return result.ToString();
+        }
+    }
+}
