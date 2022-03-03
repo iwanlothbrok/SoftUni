268dc9b39f87c00de387e481cbbe3eb0b98bc3ee
Iwan <99279234+iwanlothbrok@users.noreply.github.com>
2022-03-03 22:03:14 +0200

    Add CSharp-Advanced/GenericLab

---
+++ CSharp-Advanced/07GenericLab/BoxOfT/Box.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BoxOfT
+{
+    public class Box<T>
+    {
+        Stack<T> box;
+        public int Count
+        {
+            get
+            {
+                return box.Count;
+            }
+        }
+
+
+        public Box()
+        {
+            box = new Stack<T>();
+            
+        }
+
+        public void Add(T element)
+        {
+            box.Push(element);
+           
+        }
+
+        public T Remove()
+        {
+            if (Count == 0)
+            {
+                throw new Exception("PROBLEM ALERT!!!");
+            }
+            else
+            {
+                return box.Pop();
+            }
+
+        }
+
+
+
+    }
+}
+
---
+++ CSharp-Advanced/07GenericLab/BoxOfT/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace BoxOfT
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+            Box<int> box = new Box<int>();
+            box.Add(1);
+            
+            Console.WriteLine(box.Remove());
+            
+            
+            Console.WriteLine(box.Remove());
+        }
+    }
+}
