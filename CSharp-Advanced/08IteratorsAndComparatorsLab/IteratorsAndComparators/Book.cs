fa3e3bb916506ce75afbcb16dbc84948172990dc
Iwan <99279234+iwanlothbrok@users.noreply.github.com>
2022-03-06 19:07:00 +0200

    Add CSharp-Advanced/IteratorsAndComparatorsLab

---
+++ CSharp-Advanced/08IteratorsAndComparatorsLab/IteratorsAndComparators/Book.cs
@@ -0,0 +1,21 @@
+﻿
+
+using System.Collections.Generic;
+
+namespace IteratorsAndComparators
+{
+    public class Book
+    {
+        public Book(string title, int year,params string[] names)
+        {
+            Title = title;
+            Year = year;
+            Authors = names;
+        }
+        public string Title { get; set; }
+
+        public int Year { get; set; }
+
+        public IReadOnlyList<string> Authors;
+    }
+}
---
+++ CSharp-Advanced/08IteratorsAndComparatorsLab/IteratorsAndComparators/Library.cs
@@ -0,0 +1,16 @@
+﻿
+using System.Collections.Generic;
+
+namespace IteratorsAndComparators
+{
+    public class Library
+    {
+        private List<Book> books;
+
+
+      public Library(params Book[] books)
+        {
+            this.books = new List<Book>(books);
+        }
+    }
+}
