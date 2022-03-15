4198a2900769a93c676cc90f28b1254a6a6a6bf4
Iwan <99279234+iwanlothbrok@users.noreply.github.com>
2022-03-15 09:24:26 +0200

    Add CSharp-OOP/InheritanceLab

---
+++ CSharp-OOP/01InheritanceLab/p04RandomList/RandomList.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+
+
+namespace CustomRandomList
+{
+    public class RandomList : ArrayList
+    {
+        private Random rnd;
+
+        public RandomList()
+        {
+            this.rnd = new Random();
+        }
+        public int RandomInteger()
+        {
+            return rnd.Next();
+        }
+        public int RandomString() => this.RandomInteger();
+    }
+}
