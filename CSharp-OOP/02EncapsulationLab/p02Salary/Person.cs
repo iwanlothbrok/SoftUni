02bb518ee8f5fac643c377eef0d962f861f80c7b
Iwan <99279234+iwanlothbrok@users.noreply.github.com>
2022-03-18 22:20:26 +0200

    Add CSharp-OOP/EncapsulationLab

---
+++ CSharp-OOP/02EncapsulationLab/p02Salary/Person.cs
@@ -0,0 +1,38 @@
+﻿
+
+using System.Collections.Generic;
+
+namespace PersonsInfo
+{
+    public class Person
+    {
+        
+        public Person(string firstName, string secondName, int age, decimal salary)
+        {
+            this.FirstName = firstName;
+            this.SecondName = secondName;
+            this.Age = age;
+            this.Salary = salary;
+           
+        }
+        public string FirstName { get; private set; }
+        public string SecondName { get; private set; }
+        public int Age { get; private set; }
+        public decimal Salary { get; private set; }
+
+        public void IncreaseSalary(decimal percentage)
+        {
+            int decrease = 100;
+            if (this.Age < 30)
+            {
+                decrease = 200;
+            }
+            Salary += Salary * percentage / decrease;
+        }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {SecondName} receives {Salary:f2} leva.";
+        }
+    }
+}
---
+++ CSharp-OOP/02EncapsulationLab/p04Team/Person.cs
@@ -0,0 +1,87 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+
+namespace PersonsInfo
+{
+    public class Person
+    {
+        private string firstName;
+        private string secondName;
+        private int age;
+        private decimal salary;
+        public Person(string firstName, string secondName, int age, decimal salary)
+        {
+            this.FirstName = firstName;
+            this.SecondName = secondName;
+            this.Age = age;
+            this.Salary = salary;
+
+        }
+        public string FirstName
+        {
+            get => firstName;
+            private set
+            {
+                if (value.Length < 3)
+                {
+                    throw new ArgumentException("First name cannot contain fewer than 3 symbols!");
+                }
+                this.firstName = value;
+            }
+        }
+        public string SecondName
+        {
+            get => secondName;
+            private set
+            {
+                if (value.Length < 3)
+                {
+                    throw new ArgumentException("Last name cannot contain fewer than 3 symbols!");
+                }
+
+                this.secondName = value;
+            }
+        }
+        public int Age
+        {
+            get => age;
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Age cannot be zero or a negative integer!");
+                }
+                this.age = value;
+            }
+        }
+        public decimal Salary
+        {
+            get => salary;
+
+            private set
+            {
+                if (value < 460)
+                {
+                    throw new ArgumentException("Salary cannot be less than 650 leva!");
+                }
+                this.salary = value;
+            }
+        }
+        public void IncreaseSalary(decimal percentage)
+        {
+            int decrease = 100;
+            if (this.Age < 30)
+            {
+                decrease = 200;
+            }
+            Salary += Salary * percentage / decrease;
+        }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {SecondName} receives {Salary:f2} leva.";
+        }
+    }
+}
