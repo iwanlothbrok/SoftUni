6f201656eacbfe38c1aef2af4f054467c5996bd3
Iwan <99279234+iwanlothbrok@users.noreply.github.com>
2022-05-18 19:07:08 +0300

    Add EntityFrameworkCore/EntityFrameworkIntroductionExercise3

---
+++ EntityFrameworkCore/03.EntityFrameworkIntroduction/03. Employees Full Information/Program.cs
@@ -0,0 +1,40 @@
+﻿using SoftUni.Data;
+using System;
+using System.Linq;
+using System.Text;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var softUniContext = new SoftUniContext();
+            var result = GetEmployeesFullInformation(softUniContext);
+            Console.WriteLine(result);
+        }
+        public static string GetEmployeesFullInformation(SoftUniContext context)
+        {
+            var employees = context.Employees.Select(x => new
+            {
+                x.EmployeeId,
+                x.FirstName,
+                x.LastName,
+                x.MiddleName,
+                x.JobTitle,
+                x.Salary
+            })
+            .OrderBy(x => x.EmployeeId)
+            .ToList();
+
+            var sb = new StringBuilder();
+
+            foreach (var employee in employees)
+            {
+                sb.AppendLine($"{employee.FirstName} {employee.LastName} {employee.MiddleName} {employee.JobTitle} {employee.Salary:f2}");
+            }
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
