1cff5b954d6af37b8bea14a04aa6e4087026a2b8
Iwan <99279234+iwanlothbrok@users.noreply.github.com>
2022-05-18 19:06:49 +0300

    Add EntityFrameworkCore/EntityFrameworkIntroductionExercise2

---
+++ EntityFrameworkCore/03.EntityFrameworkIntroduction/02. Database First/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
