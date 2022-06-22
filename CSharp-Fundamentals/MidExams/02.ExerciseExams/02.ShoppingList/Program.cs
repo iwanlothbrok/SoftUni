65eadd774b393c1c7c528d9c2d63631f87aeb7f1
Iwan <99279234+iwanlothbrok@users.noreply.github.com>
2022-06-22 22:13:31 +0300

    Add Fundamentals/MidExam

---
+++ CSharp-Fundamentals/MidExams/02.ExerciseExams/02.ShoppingList/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.ShoppingList
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine()
+                .Split("!", StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+           
+
+            string cmd = Console.ReadLine();
+
+            while (cmd != "Go Shopping!")
+            {
+                var cmdArr = cmd.Split(" ");
+
+                string command = cmdArr[0];
+
+                if (command == "Urgent")
+                {
+                    if (input.Contains(cmdArr[1]) == false)
+                    {
+                        input.Insert(0,cmdArr[1]);
+                    }
+
+                }
+                else if (command == "Unnecessary")
+                {
+                    if (input.Contains(cmdArr[1]))
+                    {
+                        input.Remove(cmdArr[1]);
+                    }
+                }
+                else if (command == "Correct")
+                {
+                    if (input.Contains(cmdArr[1]))
+                    {
+                        var index = input.IndexOf(cmdArr[1]);
+                        input.Remove(cmdArr[1]);
+                        input.Insert(index, cmdArr[2]);// check if you have this item wwhat to do ?
+                    }
+                }
+                else if (command == "Rearrange")
+                {
+                    if (input.Contains(cmdArr[1]))
+                    {
+                        input.Remove(cmdArr[1]);
+                        input.Add(cmdArr[1]);
+                    }
+                }
+
+
+                cmd = Console.ReadLine();
+            }
+            Console.WriteLine(String.Join(", ", input));
+        }
+    }
+}
---
+++ CSharp-Fundamentals/MidExams/02.ExerciseExams/03.HeartDelivery/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03.HeartDelivery
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var list = Console.ReadLine()
+                     .Split("@", StringSplitOptions.RemoveEmptyEntries)
+                     .Select(int.Parse)
+                     .ToList();
+
+            var input = Console.ReadLine();
+
+            var lastIndex = 0;
+
+
+            while (input != "Love!")
+            {
+                var cmdSpl = input.Split(" ");
+                int jumpIndex = int.Parse(cmdSpl[1]);
+                jumpIndex += lastIndex;
+                if (jumpIndex > list.Count - 1 || jumpIndex < 0)
+                {
+                    jumpIndex = 0;
+                }
+
+                lastIndex = jumpIndex;
+
+                var house = list.ElementAt(jumpIndex);
+                if (house <= 0)
+                {
+                    Console.WriteLine($"Place {jumpIndex} already had Valentine's day.");
+                    input = Console.ReadLine();
+                    continue;
+                }
+                else
+                {
+                    house -= 2;
+                    list.RemoveAt(jumpIndex);
+
+                }
+
+                if (house <= 0)
+                {
+                    Console.WriteLine($"Place {jumpIndex} has Valentine's day.");
+                    house = 0;
+                }
+
+                list.Insert(jumpIndex, house);
+
+                input = Console.ReadLine();
+            }
+            int count = 0;
+            foreach (var item in list)
+            {
+                if (item != 0)
+                {
+                    count++;
+                }
+            }
+            if (count != 0)
+            {
+                Console.WriteLine($"Cupid's last position was {lastIndex}.");
+                Console.WriteLine($"Cupid has failed {count} places.");
+            }
+            else
+            {
+                Console.WriteLine($"Cupid's last position was {lastIndex}.");
+                Console.WriteLine("Mission was successful.");
+            }
+
+        }
+    }
+}
