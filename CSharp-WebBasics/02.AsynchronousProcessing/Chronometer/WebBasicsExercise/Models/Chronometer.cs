9d9063f0d5e871d5f4c0ba747e60a28f4f7299bc
Iwan <99279234+iwanlothbrok@users.noreply.github.com>
2022-07-07 22:36:36 +0300

    Add WebBasics/ChronometerExercise

---
+++ CSharp-WebBasics/02.AsynchronousProcessing/Chronometer/WebBasicsExercise/Models/Chronometer.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using WebBasicsExercise.Interfaces;
+
+namespace WebBasicsExercise.Models
+{
+
+    public class Chronometer : IChronometer
+    {
+        
+        private long milliseconds;
+        private bool isRunning;
+
+        public Chronometer()
+        {
+            this.Reset();
+        }
+
+        public string GetTime => $"{milliseconds / 60000:D2}:{(milliseconds % 60000) / 1000:D2}:{(milliseconds % 60000 % 1000):D4}";
+        public List<string> Laps { get; private set; }
+
+        public string Lap()
+        {
+            var lap = this.GetTime;
+            Laps.Add(lap);
+            return lap;
+        }
+        public void Reset()
+        {
+            this.Stop();
+            this.milliseconds = 0;
+            this.Laps = new List<string>();
+        }
+
+        public void Start()
+        {
+            this.isRunning = true;
+
+            Task.Run(() =>
+            {
+                while (isRunning)
+                {
+                    Thread.Sleep(1);
+                    this.milliseconds += 2;
+                }
+            });
+        }
+
+        public void Stop()
+        {
+            isRunning = false;
+        }
+    }
+}
+
---
+++ CSharp-WebBasics/02.AsynchronousProcessing/Chronometer/WebBasicsExercise/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using WebBasicsExercise.Models;
+
+namespace WebBasicsExercise
+{
+    public class Start
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi,write your command!");
+
+            var chronometer = new Chronometer();
+            int lapCount = -1;
+            var cmd = Console.ReadLine();
+            while (cmd != "exit")
+            {
+                if (cmd == "start")
+                {
+                    chronometer.Start();
+                }
+                else if (cmd == "stop")
+                {
+                    chronometer.Stop();
+                }
+                else if (cmd == "lap")
+                {
+
+                    lapCount++;
+                    string lap = chronometer.Lap();
+
+
+                    Console.WriteLine($"{lapCount}. {lap}");
+
+                }
+                else if (cmd == "laps")
+                {
+                    var laps = chronometer.Laps;
+                    if (laps.Count == 0)
+                    {
+                        Console.WriteLine("Laps: no laps");
+                    }
+                    else
+                    {
+                        int count = 0;
+                        Console.WriteLine("Laps:");
+                        foreach (var lap in laps)
+                        {
+                            Console.WriteLine($"{count}. {lap}");
+                            count++;
+                        }
+                    }
+                }
+                else if (cmd == "time")
+                {
+                    Console.WriteLine(chronometer.Lap());
+                }
+                else if (cmd == "reset")
+                {
+                    chronometer.Reset();
+                    lapCount = -1;
+                }
+                cmd = Console.ReadLine();
+            }
+        }
+    }
+}
