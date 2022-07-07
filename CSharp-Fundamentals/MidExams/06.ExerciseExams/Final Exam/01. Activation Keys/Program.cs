9287bfeb3e49d7451c593f8148d505ce274bd509
Iwan <99279234+iwanlothbrok@users.noreply.github.com>
2022-07-07 22:38:00 +0300

    Add Fundamentals/FinalExam

---
+++ CSharp-Fundamentals/MidExams/06.ExerciseExams/Final Exam/01. Activation Keys/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace ActivationKeys
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string activationKey = Console.ReadLine();
+
+            string command = Console.ReadLine();
+            while (command != "Generate")
+            {
+                string[] tokens = command.Split(">>>");
+                string cmdArr = tokens[0];
+                switch (cmdArr)
+                {
+                    case "Contains":
+                        string substring = tokens[1];
+                        if (activationKey.IndexOf(substring) != -1)
+                        {
+                            Console.WriteLine($"{activationKey} contains {substring}");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Substring not found!");
+                        }
+                        break;
+
+                    case "Flip":
+                        string cmd = tokens[1];
+                        int firstIndex = int.Parse(tokens[2]);
+                        int secondIndex = int.Parse(tokens[3]);
+                        string first = activationKey.Substring(0, firstIndex);
+                        string second = activationKey.Substring(firstIndex, secondIndex - firstIndex);
+                        string third = activationKey.Substring(secondIndex);
+                        if (cmd == "Upper")
+                        {
+                            second = second.ToUpper();
+                        }
+                        else
+                        {
+                            second = second.ToLower();
+                        }
+                        activationKey = first + second + third;
+                        Console.WriteLine(activationKey);
+                        break;
+                    case "Slice":
+                        int indexOne = int.Parse(tokens[1]);
+                        int indexTwo = int.Parse(tokens[2]);
+
+                        activationKey = activationKey.Remove(indexOne, indexTwo - indexOne);
+                        Console.WriteLine(activationKey);
+                        break;
+                    default:
+                        break;
+                }
+                command = Console.ReadLine();
+            }
+            Console.WriteLine($"Your activation key is: {activationKey}");
+
+        }
+    }
+}
---
+++ CSharp-Fundamentals/MidExams/06.ExerciseExams/Final Exam/03. P!rates/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace Pirates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Dictionary<string, int>> towns = new Dictionary<string, Dictionary<string, int>>();
+
+            string command = Console.ReadLine();
+            while (command != "Sail")
+            {
+                string[] tokens = command
+                    .Split("||", StringSplitOptions.RemoveEmptyEntries);
+
+                string town = tokens[0];
+                int population = int.Parse(tokens[1]);
+                int gold = int.Parse(tokens[2]);
+                if (towns.ContainsKey(town))
+                {
+                    towns[town]["population"] += population;
+                    towns[town]["gold"] += gold;
+                }
+                else
+                {
+                    towns.Add(town, new Dictionary<string, int>()
+                        {
+                        { "population",population },
+                        { "gold", gold}
+                        });
+
+                }
+                command = Console.ReadLine();
+            }
+            command = Console.ReadLine();
+            while (command != "End")
+            {
+                string[] tokens = command.Split("=>");
+                string cmdArr = tokens[0];
+                string town = tokens[1];
+                int population;
+                int gold;
+                switch (cmdArr)
+                {
+                    case "Plunder":
+
+                        population = int.Parse(tokens[2]);
+                        gold = int.Parse(tokens[3]);
+
+                        Console.WriteLine($"{town} plundered! {gold} gold stolen, {population} citizens killed.");
+                        towns[town]["population"] -= population;
+                        towns[town]["gold"] -= gold;
+                        if (towns[town]["gold"] <= 0 || towns[town]["population"] <= 0)
+                        {
+                            Console.WriteLine($"{town} has been wiped off the map!");
+                            towns.Remove(town);
+                        }
+                        break;
+
+                    case "Prosper":
+                        gold = int.Parse(tokens[2]);
+                        int sum;
+                        if (gold <= 0)
+                        {
+                            Console.WriteLine($"Gold added cannot be a negative number!");
+                            break;
+                        }
+                        else
+                        {
+                            
+                            towns[town]["gold"] += gold;
+                            sum = towns[town]["gold"];
+                        }
+                        Console.WriteLine($"{gold} gold added to the city treasury. {town} now has {sum} gold.");
+                        break;
+
+                    default:
+                        break;
+                }
+                command = Console.ReadLine();
+            }
+            Console.WriteLine($"Ahoy, Captain! There are {towns.Count} wealthy settlements to go to:");
+            foreach (var item in towns)
+            {
+                string town = item.Key;
+                int gold = towns[town]["gold"];
+                int population = towns[town]["population"];
+                Console.WriteLine($"{town} -> Population: {population} citizens, Gold: {gold} kg");
+            }
+        }
+    }
+}
