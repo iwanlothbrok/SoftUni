92ef420d264a29211c2972832ba52a3290fab805
Iwan <99279234+iwanlothbrok@users.noreply.github.com>
2022-07-31 11:16:32 +0300

    Add the new HTTP server(have problem with the debbuger) and remove the old HTTP server

--- CSharp-WebBasics/01.WebServerHTTPProtocol/BasicWebServer/BasicWebServer/HTTP/ContentType.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace BasicWebServer.Demo.HTTP
-{
-    public class ContentType
-    {
-        public const string PlaintText = "text/plaint; charset=UTF-8";
-        public const string Html= "text/html; charset=UTF-8";
-    }
-}
