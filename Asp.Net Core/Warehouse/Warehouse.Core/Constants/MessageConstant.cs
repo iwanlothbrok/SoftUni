5d5c0b59796879f8175b6620dc21bc2c1f31c877
Iwan <99279234+iwanlothbrok@users.noreply.github.com>
2022-09-05 09:39:15 +0300

    add in use toastr

---
+++ Asp.Net Core/Warehouse/Warehouse.Core/Constants/MessageConstant.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Warehouse.Core.Constants
+{
+    public class MessageConstant
+    {
+        public const string ErrorMessage = "ErrorMessage";
+
+        public const string SuccsessMessage = "SuccsessMessage";
+
+        public const string WarningMessage = "SuccsessMessage";
+    }
+}
