2cf00f407311b219069e056509c5809074fa8bbe
Iwan <99279234+iwanlothbrok@users.noreply.github.com>
2022-09-07 20:11:39 +0300

    add decimal model binder / and start model binder provider

---
+++ Asp.Net Core/Warehouse/Warehouse/ModelBinders/DecimalModelBinder.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.Globalization;
+
+namespace Warehouse.ModelBinders
+{
+    public class DecimalModelBinder : IModelBinder
+    {
+        public Task BindModelAsync(ModelBindingContext bindingContext)
+        {
+            ValueProviderResult valueResult = bindingContext
+                             .ValueProvider
+                             .GetValue(bindingContext.ModelName);
+
+            if (valueResult != ValueProviderResult.None && String.IsNullOrWhiteSpace(valueResult.FirstValue) == false)
+            {
+                decimal actualValue = 0;
+                bool succsess = false;
+
+                try
+                {
+                    string decimalValue = valueResult.FirstValue;
+                    decimalValue = decimalValue.Replace(",", CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator);
+                    decimalValue = decimalValue.Replace(".", CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator);
+
+                    actualValue = Convert.ToDecimal(decimalValue, CultureInfo.CurrentCulture);
+                    succsess = true;
+
+
+                }
+                catch (FormatException fe)
+                {
+                    bindingContext.ModelState.AddModelError(bindingContext.ModelName, fe, bindingContext.ModelMetadata);
+
+                }
+
+                if (succsess)
+                {
+                    bindingContext.Result = ModelBindingResult.Success(actualValue);
+
+                }
+
+            }
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ Asp.Net Core/Warehouse/Warehouse/ModelBinders/DecimalModelBinderProvider.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace Warehouse.ModelBinders
+{
+    public class DecimalModelBinderProvider : IModelBinderProvider
+    {
+        public IModelBinder? GetBinder(ModelBinderProviderContext context)
+        {
+           if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+        }
+    }
+}
