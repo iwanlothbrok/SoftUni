b01dd125fec8cf26644dbab5c24a8207375694b4
Iwan <99279234+iwanlothbrok@users.noreply.github.com>
2023-03-08 12:31:51 +0200

    OneTimeSetUpBase is ready

--- Appium_Exercise/Eventures.DesktopApp.AppiumTests/AppiumTestsBase.cs
+++ Appium_Exercise/Eventures.DesktopApp.AppiumTests/AppiumTestsBase.cs
@@ -10,19 +10,57 @@ using OpenQA.Selenium.Appium;
 using OpenQA.Selenium.Appium.Service;
 using OpenQA.Selenium.Appium.Windows;
 using OpenQA.Selenium.Support.UI;
+using Microsoft.OpenApi.Models;
+using System.Net.WebSockets;
 
 namespace Eventures.DesktopApp.AppiumTests
 {
-    public class AppiumTestsBase
-    {        
-        [OneTimeSetUp]
-        public void OneTimeSetUpBase()
-        {
-        }
+	public class AppiumTestsBase
+	{
+		protected TestDb testDb;
+		protected ApplicationDbContext dbContext;
+		private TestEventuresApp<Startup> testEventuresApp;
+		protected string baseUrl;
+		private AppiumLocalService appiumLocalService;
+		private string ApiPath = @"../../../../Eventures.WebAPI";
+		private string AppPath = @"../../../../Eventures.DesktopApp/bin/Debug" +
+								 @"/net5.0-windows/Eventures.DesktopApp.exe";
+		protected WindowsDriver<WindowsElement> driver;
+		protected WebDriverWait wait;
 
-        [OneTimeTearDown]
-        public void OneTimeTearDownBase()
-        {
-        }
-    }
+
+
+		[OneTimeSetUp]
+		public void OneTimeSetUpBase()
+		{
+			//creating
+			this.testDb = new TestDb();
+			this.dbContext = testDb.CreateDbContext();
+			this.testEventuresApp = new TestEventuresApp<Startup>(testDb, ApiPath);
+			this.baseUrl = this.testEventuresApp.ServerUri;
+
+			// implement and start service 
+			appiumLocalService = new AppiumServiceBuilder().UsingAnyFreePort().Build();
+			appiumLocalService.Start();
+
+			// connecting appium
+			var appiumOptions = new AppiumOptions() { PlatformName = "Windows" };
+			var fullPathName = Path.GetFullPath(AppPath);
+			appiumOptions.AddAdditionalCapability("app", fullPathName);
+
+			// setting the options and initializating drivers
+			driver = new WindowsDriver<WindowsElement>(appiumLocalService, appiumOptions);
+
+			// setting implicit WAIT 
+			driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+
+			// explecit WAIT
+			wait = new WebDriverWait(driver, TimeSpan.FromSeconds(60));
+		}
+
+		[OneTimeTearDown]
+		public void OneTimeTearDownBase()
+		{
+		}
+	}
 }
