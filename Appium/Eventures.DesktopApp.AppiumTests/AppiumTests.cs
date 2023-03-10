2edf950519ebca42061e3fb822eb8d2bed939a97
Iwan <99279234+iwanlothbrok@users.noreply.github.com>
2023-03-10 11:52:08 +0200

    half of appium test are ready

---
+++ Appium/Eventures.DesktopApp.AppiumTests/AppiumTests.cs
@@ -0,0 +1,245 @@
+using System;
+using System.Linq;
+
+using NUnit.Framework;
+
+namespace Eventures.DesktopApp.AppiumTests
+{
+	public class AppiumTests : AppiumTestsBase
+	{
+		private string username = "user" + DateTime.Now.Ticks.ToString().Substring(10);
+		private string password = "pass" + DateTime.Now.Ticks.ToString().Substring(10);
+		private const string EventBoardWindowName = "Event Board";
+		private const string CreateWindowNewEvent = "Create a New Event";
+		private const string RegisterAUserWindowName = "Register a New User";
+
+		[Test, Order(1)]
+		public void Test_Connect_WithEmptyUrl()
+		{
+			// locating url
+			var apiUrlField = driver.FindElementByAccessibilityId("textBoxApiUrl");
+			apiUrlField.Clear();
+
+			//locating and clicking btn 
+			var connectBtn = driver.FindElementByAccessibilityId("buttonConnect");
+			connectBtn.Click();
+
+			//wait and assert 
+			var windowAppears = this.wait
+				.Until(s => driver.PageSource.Contains("Connect to Eventures API"));
+			Assert.IsTrue(windowAppears);
+
+			var statusTextBox = driver.FindElementByXPath("/Window/StatusBar/Text");
+
+			var messageAppears = this.wait
+				.Until(s => statusTextBox.Text.Contains("Error: Value cannot be null."));
+			Assert.IsTrue(messageAppears);
+		}
+
+		[Test, Order(2)]
+		public void Test_Connect_WithInvalidUrl()
+		{
+			// locating url
+			var apiUrlField = driver.FindElementByAccessibilityId("textBoxApiUrl");
+			string apiUrl = apiUrlField.Text;
+
+			UriBuilder builder = new UriBuilder(apiUrl);
+			builder.Port = 7591; // Change the port number to 8080
+			string newApiUrl = builder.Uri.ToString();
+
+			apiUrlField.SendKeys(newApiUrl);
+
+			//locating and clicking btn 
+			var connectBtn = driver.FindElementByAccessibilityId("buttonConnect");
+			connectBtn.Click();
+
+			//wait and assert 
+			var windowAppears = this.wait
+				.Until(s => driver.PageSource.Contains("Connect to Eventures API"));
+			Assert.IsTrue(windowAppears);
+
+
+			var statusTextBox = driver.FindElementByXPath("/Window/StatusBar/Text");
+			var messageAppears = this.wait
+				.Until(s => statusTextBox.Text.Contains("Error: HTTP error `No connection"));
+			Assert.IsTrue(messageAppears);
+		}
+
+		[Test, Order(3)]
+		public void Test_Connect_WithValidUrl()
+		{
+			// locating url
+			var apiUrlField = driver.FindElementByAccessibilityId("textBoxApiUrl");
+			apiUrlField.Clear();
+			apiUrlField.SendKeys("http://127.0.0.1/api");
+
+			//locating and clicking btn 
+			var connectBtn = driver.FindElementByAccessibilityId("buttonConnect");
+			connectBtn.Click();
+
+			//wait and assert 
+			var windowAppears = this.wait
+				.Until(s => driver.PageSource.Contains(EventBoardWindowName));
+			Assert.IsTrue(windowAppears);
+
+			var statusTextBox = driver.FindElementByXPath("/Window/StatusBar/Text");
+			var messageAppears = this.wait
+				.Until(s => statusTextBox.Text.Contains("Connected to the Web API."));
+			Assert.IsTrue(messageAppears);
+		}
+
+		[Test, Order(4)]
+		public void Test_Register_Invalid()
+		{
+			// getting btns
+			var createBtn = driver.FindElementByAccessibilityId("buttonCreate");
+			Assert.IsFalse(createBtn.Enabled);
+			var reloadBtn = driver.FindElementByAccessibilityId("buttonReload");
+			Assert.IsFalse(reloadBtn.Enabled);
+
+			// register 
+			var registerBtn = driver.FindElementByAccessibilityId("buttonRegister");
+			registerBtn.Click();
+
+
+			//textBoxUsername
+			var usernameField = driver.FindElementByAccessibilityId("textBoxUsername");
+			usernameField.Clear();
+
+			//email
+			var emailField = driver.FindElementByAccessibilityId("textBoxEmail");
+			emailField.Clear();
+
+			//pass
+			var passwordField = driver.FindElementByAccessibilityId("textBoxPassword");
+			passwordField.Clear();
+			passwordField.SendKeys(this.password);
+
+			//confirm pass
+			var confirmPassField = driver.FindElementByAccessibilityId("textBoxConfirmPassword");
+			confirmPassField.Clear();
+			confirmPassField.SendKeys(this.password);
+
+			//first name
+			var firstNameField = driver.FindElementByAccessibilityId("textBoxFirstName");
+			firstNameField.Clear();
+			firstNameField.SendKeys("User");
+
+			//last name
+			var lastNameField = driver.FindElementByAccessibilityId("textBoxLastName");
+			lastNameField.Clear();
+			lastNameField.SendKeys("Userov");
+
+
+			//last name
+			var confirm = driver.FindElementByAccessibilityId("buttonRegisterConfirm");
+			confirm.Click();
+
+			// check if its right 
+			Assert.True(driver.PageSource.Contains(EventBoardWindowName));
+
+
+			var statusTextBox = driver.FindElementByXPath("/Window/StatusBar/Text");
+			var messageAppears = this.wait
+				//check this message 
+				.Until(s => statusTextBox.Text.Contains("Connected to the Web API."));
+			Assert.IsTrue(messageAppears);
+
+			var eventsInDb = this.dbContext.Events.Count();
+
+			var asd = this.wait
+			.Until(s => statusTextBox.Text.Contains("Error: Value cannot be null."));
+			Assert.IsTrue(messageAppears);
+
+			Assert.IsTrue(createBtn.Enabled);
+			Assert.IsTrue(reloadBtn.Enabled);
+		}
+
+		[Test, Order(5)]
+		public void Test_Register()
+		{
+			// getting btns
+			var createBtn = driver.FindElementByAccessibilityId("buttonCreate");
+			Assert.IsFalse(createBtn.Enabled);
+			var reloadBtn = driver.FindElementByAccessibilityId("buttonReload");
+			Assert.IsFalse(reloadBtn.Enabled);
+
+			// register 
+			var registerBtn = driver.FindElementByAccessibilityId("buttonRegister");
+			registerBtn.Click();
+
+
+			//textBoxUsername
+			var usernameField = driver.FindElementByAccessibilityId("textBoxUsername");
+			usernameField.Clear();
+			usernameField.SendKeys(this.username);
+
+			//email
+			var emailField = driver.FindElementByAccessibilityId("textBoxEmail");
+			emailField.Clear();
+			emailField.SendKeys(this.username + "@mail.com");
+
+			//pass
+			var passwordField = driver.FindElementByAccessibilityId("textBoxPassword");
+			passwordField.Clear();
+			passwordField.SendKeys(this.password);
+
+			//confirm pass
+			var confirmPassField = driver.FindElementByAccessibilityId("textBoxConfirmPassword");
+			confirmPassField.Clear();
+			confirmPassField.SendKeys(this.password);
+
+			//first name
+			var firstNameField = driver.FindElementByAccessibilityId("textBoxFirstName");
+			firstNameField.Clear();
+			firstNameField.SendKeys("User");
+
+			//last name
+			var lastNameField = driver.FindElementByAccessibilityId("textBoxLastName");
+			lastNameField.Clear();
+			lastNameField.SendKeys("Userov");
+
+
+			//last name
+			var confirm = driver.FindElementByAccessibilityId("buttonRegisterConfirm");
+			confirm.Click();
+
+			// check if its right 
+			Assert.True(driver.PageSource.Contains(EventBoardWindowName));
+
+
+			var statusTextBox = driver.FindElementByXPath("/Window/StatusBar/Text");
+			var messageAppears = this.wait
+				//check this message 
+				.Until(s => statusTextBox.Text.Contains("Connected to the Web API."));
+			Assert.IsTrue(messageAppears);
+
+			var eventsInDb = this.dbContext.Events.Count();
+
+			Assert.AreEqual($"Load succsessful: {eventsInDb} events loaded.", statusTextBox.Text);
+
+			Assert.IsTrue(createBtn.Enabled);
+			Assert.IsTrue(reloadBtn.Enabled);
+		}
+
+		[Test, Order(6)]
+		public void Test_Login_Invalid()
+		{
+		}
+
+		[Test, Order(7)]
+		public void Test_Login()
+		{
+		}
+
+		[Test]
+		public void Test_CreateEvent()
+		{
+		}
+
+		[Test]
+		public void Test_CreateEvent_Invalid()
+		{
+		}
+	}
+}
---
+++ Appium/Eventures.DesktopApp.AppiumTests/AppiumTestsBase.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.IO;
+
+using Eventures.Data;
+using Eventures.WebAPI;
+using Eventures.Tests.Common;
+
+using NUnit.Framework;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Service;
+using OpenQA.Selenium.Appium.Windows;
+using OpenQA.Selenium.Support.UI;
+
+namespace Eventures.DesktopApp.AppiumTests
+{
+	public class AppiumTestsBase
+	{
+		protected TestDb testDb;
+		protected ApplicationDbContext dbContext;
+		private TestEventuresApp<Startup> testEventuresApp;
+		protected string baseUrl;
+		private AppiumLocalService appiumLocalService;
+		private string ApiPath = @"../../../../Eventures.WebAPI";
+		private string AppPath = @"../../../../Eventures.DesktopApp/bin/Debug" +
+								 @"/net5.0-windows/Eventures.DesktopApp.exe";
+		protected WindowsDriver<WindowsElement> driver;
+		protected WebDriverWait wait;
+
+		[OneTimeSetUp]
+		public void OneTimeSetUpBase()
+		{
+			//creating
+			this.testDb = new TestDb();
+			this.dbContext = testDb.CreateDbContext();
+			this.testEventuresApp = new TestEventuresApp<Startup>(testDb, ApiPath);
+			this.baseUrl = this.testEventuresApp.ServerUri;
+
+			// implement and start service 
+			appiumLocalService = new AppiumServiceBuilder().UsingAnyFreePort().Build();
+			appiumLocalService.Start();
+
+			// connecting appium
+			var appiumOptions = new AppiumOptions() { PlatformName = "Windows" };
+			var fullPathName = Path.GetFullPath(AppPath);
+			appiumOptions.AddAdditionalCapability("app", fullPathName);
+
+			// setting the options and initializating drivers
+			driver = new WindowsDriver<WindowsElement>(appiumLocalService, appiumOptions);
+
+			// setting implicit WAIT 
+			driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+
+			// explecit WAIT
+			wait = new WebDriverWait(driver, TimeSpan.FromSeconds(60));
+		}
+
+		[OneTimeTearDown]
+		public void OneTimeTearDownBase()
+		{
+			//closing and quiting the app
+			driver.CloseApp();
+			driver.Quit();
+
+			//dispose 
+			appiumLocalService.Dispose();
+
+			//dispose the api
+			this.testEventuresApp.Dispose();
+		}
+	}
+}
